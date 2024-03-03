42db18b48d9b7696fa9b6e43bec55babfe2247be
llihwerd <100llihwerd@gmail.com>
2024-03-02 20:11:49 -0600

    integrate test plant cards and set up structure for all plants page

--- main_app/views.py
+++ main_app/views.py
@@ -9,3 +9,18 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+
+class Plant:
+  def __init__(self, name, species, description):
+    self.name = name
+    self.species = species
+    self.description = description
+
+plants = [
+  Plant('Farah', 'fern', 'Long & leafy'),
+  Plant('Carrie', 'cactus', 'Short & sharp'),
+]
+
+def plant_index(request):
+  return render(request, 'plants/index.html', { 'plants': plants })
