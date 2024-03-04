014a1127b5b8f3b16b77fbdabecdc40fe0d25b41
llihwerd <100llihwerd@gmail.com>
2024-03-04 14:49:34 -0600

    add plant detail

--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
 from .models import Plant
 
-# Add the following import
-from django.http import HttpResponse
 
-# Define the home view
+
 def home(request):
-  return HttpResponse('<h1>Hello plants')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
-
-
 def plant_index(request):
   plants = Plant.objects.all()
   return render(request, 'plants/index.html', { 'plants': plants })
+
+def plant_detail(request, plant_id):
+  plant = Plant.objects.get(id=plant_id)
+  return render(request, 'plants/detail.html', { 'plant': plant })
