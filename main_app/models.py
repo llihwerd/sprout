c1bc659737b4baf0d14e6e97d7d43d2a3c4c54fe
llihwerd <100llihwerd@gmail.com>
2024-03-04 15:14:25 -0600

    add plant_confirm_delete form

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Plant(models.Model):
   name = models.CharField(max_length=100)
@@ -9,3 +10,6 @@ class Plant(models.Model):
 
   def __str__(self):
     return self.name
+  
+  def get_absolute_url(self):
+    return reverse('plant-detail', kwargs={'plant_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Plant
 
 
@@ -21,3 +21,12 @@ def plant_detail(request, plant_id):
 class PlantCreate(CreateView):
   model = Plant
   fields = '__all__'
+  success_url = '/plants/'
+
+class PlantUpdate(UpdateView):
+  model = Plant
+  fields = ['species', 'description']
+
+class PlantDelete(DeleteView):
+  model = Plant
+  success_url = '/plants/'
