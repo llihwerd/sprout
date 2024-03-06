900f2985b96856b696f9526de9ab022a12425e9f
llihwerd <100llihwerd@gmail.com>
2024-03-05 22:26:53 -0600

    fix styling issue with logo

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 class Plant(models.Model):
   name = models.CharField(max_length=100)
@@ -7,6 +8,7 @@ class Plant(models.Model):
   description = models.TextField(max_length=250)
   waterNeeded = models.TextField(max_length=250)
   sunHoursNeeded = models.IntegerField()
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
 
   def __str__(self):
     return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,32 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Plant
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 
-def home(request):
-  return render(request, 'home.html')
+class Home(LoginView):
+  template_name = 'home.html'
 
 def about(request):
   return render(request, 'about.html')
 
+@login_required
 def plant_index(request):
-  plants = Plant.objects.all()
+  plants = Plant.objects.filter(user=request.user)
   return render(request, 'plants/index.html', { 'plants': plants })
 
+@login_required
 def plant_detail(request, plant_id):
   plant = Plant.objects.get(id=plant_id)
   return render(request, 'plants/detail.html', { 'plant': plant })
 
-class PlantCreate(CreateView):
+class PlantCreate(LoginRequiredMixin, CreateView):
   model = Plant
-  fields = '__all__'
+  fields = ['name', 'species', 'description', 'waterNeeded', 'sunHoursNeeded']
+  success_url = '/plants/'
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
+
+class PlantUpdate(LoginRequiredMixin, UpdateView):
+  model = Plant
+  fields = fields = ['name', 'species', 'description', 'waterNeeded', 'sunHoursNeeded']
+
+class PlantDelete(LoginRequiredMixin, DeleteView):
+  model = Plant
   success_url = '/plants/'
 
-class PlantUpdate(UpdateView):
-  model = Plant
-  fields = ['species', 'description']
-
-class PlantDelete(DeleteView):
-  model = Plant
-  success_url = '/plants/'
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('plant-index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
